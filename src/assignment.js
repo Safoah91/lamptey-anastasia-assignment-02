4c5c689e4fabc6c681eb076c55eb04944a613bb2
Anastasia Safoah Lamptey <104417812+Safoah91@users.noreply.github.com>
2022-07-07 13:27:28 +0000

    attempted challenge - 1 & challenge - 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  }
+  
+    return `Total sum is: ${sum}`;
+  //arrayOfNumbers.length
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+//assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const even = [];
+
+  arrayOfNumbers.forEach(number => {
+    if (number % 2 === 0) {
+      even.push(number);
+    }
+  });
+
+  return `There are ${even.length} even numbers is the array`;
+    //return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
